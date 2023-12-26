4137022cd9379d41c833c41be2dfd5da65257fb5
Suprim Shrestha <suprim.shresth@gmail.com>
2023-12-26 22:00:29 +0545

    Use insert instead of copy from csv

--- day-1/sql/seeds/3Shippers.sql
+++ day-1/sql/seeds/3Shippers.sql
@@ -1,5 +1,3 @@
-CREATE TABLE IF NOT EXISTS Shippers (
-	shipper_id INT PRIMARY KEY,
-	shipper_name VARCHAR(255),
-	phone VARCHAR(20)
-);
+INSERT INTO shippers VALUES (1, 'Speedy Express', '(503) 555-9831');
+INSERT INTO shippers VALUES (2, 'United Package', '(503) 555-3199');
+INSERT INTO shippers VALUES (3, 'Federal Shipping', '(503) 555-9931');
--- day-1/sql/seeds/5Categories.sql
+++ day-1/sql/seeds/5Categories.sql
@@ -1,1 +1,8 @@
-\COPY Categories FROM '../csv/Categories.csv' WITH CSV HEADER;
+INSERT INTO categories VALUES (1, 'Beverages', 'Soft drinks, coffees, teas, beers, and ales');
+INSERT INTO categories VALUES (2, 'Condiments', 'Sweet and savory sauces, relishes, spreads, and seasonings');
+INSERT INTO categories VALUES (3, 'Confections', 'Desserts, candies, and sweet breads');
+INSERT INTO categories VALUES (4, 'Dairy Products', 'Cheeses');
+INSERT INTO categories VALUES (5, 'Grains/Cereals', 'Breads, crackers, pasta, and cereal');
+INSERT INTO categories VALUES (6, 'Meat/Poultry', 'Prepared meats');
+INSERT INTO categories VALUES (7, 'Produce', 'Dried fruit and bean curd');
+INSERT INTO categories VALUES (8, 'Seafood', 'Seaweed and fish');
